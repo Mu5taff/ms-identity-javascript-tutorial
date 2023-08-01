656a94ddcc373b4c6663113d646a71508defd64a
Mustaf Mohamed <mustafsaney1998@gmail.com>
2023-08-01 11:37:44 +0100

    authRedirect Checked +

--- 1-Authentication/1-sign-in/App/authConfig.js
+++ 1-Authentication/1-sign-in/App/authConfig.js
@@ -1,49 +1,50 @@
 /**
- * Configuration object to be passed to MSAL instance on creation. 
+ * Configuration object to be passed to MSAL instance on creation.
  * For a full list of MSAL.js configuration parameters, visit:
- * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
  */
 
 const msalConfig = {
-    auth: {
-        clientId: 'Enter_the_Application_Id_Here', // This is the ONLY mandatory field that you need to supply.
-        authority: 'https://login.microsoftonline.com/Enter_the_Tenant_Info_Here', // Defaults to "https://login.microsoftonline.com/common"
-        redirectUri: '/', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.href e.g. http://localhost:3000/
-        navigateToLoginRequestUrl: true, // If "true", will navigate back to the original request location before processing the auth code response.
+  auth: {
+    clientId: "e589a800-6ac6-47f7-8702-e7e2dc0aedac", // This is the ONLY mandatory field that you need to supply.
+    authority:
+      "https://login.microsoftonline.com/7eb155e4-aba7-499a-b995-6fb209044bca", // Defaults to "https://login.microsoftonline.com/common"
+    redirectUri: "/", // You must register this URI on Azure Portal/App Registration. Defaults to window.location.href e.g. http://localhost:3000/
+    navigateToLoginRequestUrl: true, // If "true", will navigate back to the original request location before processing the auth code response.
+  },
+  cache: {
+    cacheLocation: "sessionStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO.
+    storeAuthStateInCookie: false, // set this to true if you have to support IE
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case msal.LogLevel.Error:
+            console.error(message);
+            return;
+          case msal.LogLevel.Info:
+            console.info(message);
+            return;
+          case msal.LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case msal.LogLevel.Warning:
+            console.warn(message);
+            return;
+        }
+      },
     },
-    cache: {
-        cacheLocation: 'sessionStorage', // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO.
-        storeAuthStateInCookie: false, // set this to true if you have to support IE
-    },
-    system: {
-        loggerOptions: {
-            loggerCallback: (level, message, containsPii) => {
-                if (containsPii) {
-                    return;
-                }
-                switch (level) {
-                    case msal.LogLevel.Error:
-                        console.error(message);
-                        return;
-                    case msal.LogLevel.Info:
-                        console.info(message);
-                        return;
-                    case msal.LogLevel.Verbose:
-                        console.debug(message);
-                        return;
-                    case msal.LogLevel.Warning:
-                        console.warn(message);
-                        return;
-                }
-            },
-        },
-    },
+  },
 };
 
 /**
  * Scopes you add here will be prompted for user consent during sign-in.
  * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
- * For more information about OIDC scopes, visit: 
+ * For more information about OIDC scopes, visit:
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 const loginRequest = {
@@ -61,9 +62,9 @@ const loginRequest = {
 // };
 
 // exporting config object for jest
-if (typeof exports !== 'undefined') {
+if (typeof exports !== "undefined") {
   module.exports = {
-      msalConfig: msalConfig,
-      loginRequest: loginRequest,
+    msalConfig: msalConfig,
+    loginRequest: loginRequest,
   };
 }
